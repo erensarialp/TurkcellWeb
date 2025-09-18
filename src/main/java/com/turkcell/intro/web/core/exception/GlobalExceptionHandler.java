02d82916d3b5448a32e92ed4bbc2fdd012472ab8
Eren Sarialp <erensrlp@gmail.com>
2025-09-18 19:57:56 +0300

    ExceptionHandler is added.

---
+++ src/main/java/com/turkcell/intro/web/core/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.turkcell.intro.web.core.exception;
+
+import com.turkcell.intro.web.core.exception.type.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler
+{
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handeValidationException()
+    {
+        return "Validation Error";
+    }
+
+    //Her exception handler firlayan exceptionu'i parametre olarak alir.
+    @ExceptionHandler(BusinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleBusinessException(BusinessException e)
+    {
+        return e.getMessage();
+    }
+
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRuntimeException()
+    {
+        return "Runtime Error";
+    }
+}
